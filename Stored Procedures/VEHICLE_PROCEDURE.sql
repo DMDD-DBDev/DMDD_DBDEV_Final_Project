11ff3959b544fc2180fb658d538778847cdd7c6e
KunalBhoyar <kunal@Kunals-MacBook-Air.local>
2023-11-30 20:14:55 -0500

    Final Script

--- Stored Procedures/VEHICLE_PROCEDURE.sql
+++ Stored Procedures/VEHICLE_PROCEDURE.sql
@@ -8,13 +8,26 @@ CREATE OR REPLACE PROCEDURE add_vehicle (
     IN_CONTACT_NO NUMBER
 ) AS
     V_AGENT_ID NUMBER;
-    v_count NUMBER;
+    v_count_id NUMBER;
+    v_count_number NUMBER;
     e_vehicle_exists EXCEPTION;
+    v_seq_val NUMBER; -- Variable to store the current sequence value
 BEGIN
-    -- Check if the VEHICLE_NUMBER already exists
-    SELECT COUNT(1) INTO v_count FROM VEHICLE WHERE VEHICLE_NUMBER = IN_VEHICLE_NUMBER;
+    -- Store the current sequence value in a variable
+    SELECT DELIVERY_VEHICLE_SEQ.NEXTVAL INTO v_seq_val FROM DUAL;
 
-    IF v_count > 0 THEN
+    -- Check if the VEHICLE_ID already exists
+    SELECT COUNT(1) INTO v_count_id FROM VEHICLE WHERE VEHICLE_ID = v_seq_val;
+
+    IF v_count_id > 0 THEN
+        -- If the vehicle ID already exists, raise an exception
+        RAISE e_vehicle_exists;
+    END IF;
+
+    -- Check if the VEHICLE_NUMBER already exists
+    SELECT COUNT(1) INTO v_count_number FROM VEHICLE WHERE VEHICLE_NUMBER = IN_VEHICLE_NUMBER;
+
+    IF v_count_number > 0 THEN
         -- If the vehicle number already exists, raise an exception
         RAISE e_vehicle_exists;
     END IF;
@@ -22,9 +35,9 @@ BEGIN
     -- Attempt to fetch the AGENT_ID from DELIVERY_AGENT table
     SELECT AGENT_ID INTO V_AGENT_ID FROM DELIVERY_AGENT WHERE CONTACT_NO = IN_CONTACT_NO;
 
-    -- Insert data into the VEHICLE table using the sequence for VEHICLE_ID
+    -- Insert data into the VEHICLE table using the stored sequence value for VEHICLE_ID
     INSERT INTO VEHICLE (VEHICLE_ID, VEHICLE_NUMBER, TYPE, AGENT_ID)
-    VALUES (DELIVERY_VEHICLE_SEQ.NEXTVAL, IN_VEHICLE_NUMBER, INITCAP(IN_TYPE), V_AGENT_ID);
+    VALUES (v_seq_val, IN_VEHICLE_NUMBER, INITCAP(IN_TYPE), V_AGENT_ID);
 
     -- Commit the transaction
     COMMIT;
@@ -32,7 +45,7 @@ BEGIN
 
 EXCEPTION
     WHEN e_vehicle_exists THEN
-        DBMS_OUTPUT.PUT_LINE('VEHICLE NUMBER Already Exists');
+        DBMS_OUTPUT.PUT_LINE('VEHICLE ID or NUMBER Already Exists');
     WHEN NO_DATA_FOUND THEN
         -- Handle exception if no data is found
         DBMS_OUTPUT.PUT_LINE('Vehicle not found');
@@ -40,13 +53,14 @@ EXCEPTION
         -- Handle other exceptions and print the error message
         DBMS_OUTPUT.PUT_LINE(SQLERRM);
 END add_vehicle;
+/
     
     
   
   
 -- Execute add_vehicle procedure with dummy data
 
-EXEC add_vehicle(121, 'Compact', 9876543210);
+EXEC add_vehicle(122221, 'Compact', 9876543210);
   
 
 
